927a7dc959a9b7e8dd6834d0c1386bf141f22a5e
JuniorMaia2394 <maiajr2394@gmail.com>
2022-04-01 23:45:06 -0300

    begin crud loja

---
+++ resources/views/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastrar produto</title>
+</head>
+<body>
+    <form action="">
+        <label for="">Nome produto</label><br />
+        <input type="text" name="nome"><br />
+        <label for="">Imagem produto</label><br />
+        <input type="image" name="imagem"><br />
+        <label for="">Categoria</label><br />
+        <input type="text" name="categoria"><br />
+        <label for="">Desenvolvedora</label><br />
+        <input type="text" name="desenvolvedora"><br />
+        <label for="">Preço</label><br />
+        <input type="number" name="preço"><br />
+        <label for="">Comentário</label><br />
+        <input type="text" name="comentario"><br />
+    </form>
+</body>
+</html>
